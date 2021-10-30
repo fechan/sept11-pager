780303232e41f8b0c59cc572d51e5d40fb8a9291
fechan <56131910+fechan@users.noreply.github.com>
2021-10-30 00:57:18 -0700

    add USE msg similarity script

---
+++ msg_similarity_use.py
@@ -0,0 +1,81 @@
+import csv
+import pickle
+import os
+import os.path
+import time
+from sklearn.metrics.pairwise import cosine_similarity
+import tensorflow_hub as hub
+
+MESSAGES = list(csv.reader(open("messages_all.txt.alphaonly.100.csv", "r")))
+SCRATCH_DIR = "./scratch"
+
+model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+
+deemed_similar = set() # message numbers of all messages already deemed similar to another message
+similarity_families = {} # sets of messages similar to a base message, INCLUDING itself
+
+similarity_family_file = open("alpha_similarity_families.txt", "w")
+
+def try_exact_match(base_msg_no, text):
+    """Return a list of message numbers whose message contents match the given text exactly
+    that have not already been deemed similar to another message"""
+    matches = []
+    for message in MESSAGES:
+        if (message[0] == base_msg_no) or (message[0] in deemed_similar):
+            continue
+        if message[10] == text:
+            matches.append(message[0])
+    return matches
+
+def add_to_similarity_family(base_msg_no, msg_no):
+    """Add the given message number to the similarity family of the given base message number"""
+    if base_msg_no not in similarity_families:
+        similarity_families[base_msg_no] = set()
+    similarity_families[base_msg_no].add(msg_no)
+
+def get_embedding_with_caching(message_no, message_content):
+    """Get the embedding for the given message and save it to scratch/cache,
+    or if it has already been calculated, return the cached embedding"""
+    if os.path.isfile(f"{SCRATCH_DIR}/embedding.{message_no}.pkl"):
+        with open(f"{SCRATCH_DIR}/embedding.{message_no}.pkl", "rb") as picklefile:
+            return pickle.load(picklefile)
+    else:
+        embedding = model(message_content)
+        with open(f"{SCRATCH_DIR}/embedding.{message_no}.pkl", "wb") as picklefile:
+            pickle.dump(embedding, picklefile)
+        return embedding
+
+for message in MESSAGES:
+    if message[0] in deemed_similar: continue
+
+    print(f"Base message {message[0]}: {message[10]}")
+    embedding = get_embedding_with_caching(message[0], message[10])
+    have_tried_exact_match = False # Whether we've already tried to find an exact matches for this base message
+
+    for other_message in MESSAGES:
+        if (message[0] == other_message[0]) or (other_message[0] in deemed_similar): continue
+
+        # If there's already been a match, it's more likely that there are
+        # matches that are exactly the same
+        if (not have_tried_exact_match) and (message[10] == other_message[10]):
+            for msg in try_exact_match(message[0], message[10]):
+                add_to_similarity_family(message[0], msg)
+                print(f"Similar message {other_message[0]}: {other_message[10]}")
+            add_to_similarity_family(message[0], message[0])
+            have_tried_exact_match = True
+            continue # don't compute cosine similarity for other_message since we know it's the same
+
+        other_embedding = get_embedding_with_caching(other_message[0], other_message[10])
+        if cosine_similarity(embedding, other_embedding)[0][0] > 0.8:
+            print(f"Similar message {other_message[0]}: {other_message[10]}")
+
+            deemed_similar.add(message[0])
+            deemed_similar.add(other_message[0])
+            add_to_similarity_family(message[0], other_message[0])
+            add_to_similarity_family(message[0], message[0])
+    if message[0] in similarity_families:
+        similarity_family_file.write(f"{message[0]}\t{','.join(similarity_families[message[0]])}\n")
+        similarity_family_file.flush() # ensure the file is written to disk immediately
+        os.fsync(similarity_family_file.fileno())
+
+similarity_family_file.close()
