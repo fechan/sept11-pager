4d534914e0f380f563fcb94c3fc1953308be176d
fechan <56131910+fechan@users.noreply.github.com>
2022-03-05 22:08:26 -0800

    add similarity optimizations (unused in the end)

--- msg_similarity_use.py
+++ msg_similarity_use.py
@@ -26,17 +26,6 @@ if os.path.isfile("alpha_similarity_families.txt"):
 model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 similarity_family_file = open("alpha_similarity_families.txt", "a")
 
-def try_exact_match(base_msg_no, text):
-    """Return a list of message numbers whose message contents match the given text exactly
-    that have not already been deemed similar to another message"""
-    matches = []
-    for message in MESSAGES:
-        if (message[0] == base_msg_no) or (message[0] in deemed_similar):
-            continue
-        if message[10] == text:
-            matches.append(message[0])
-    return matches
-
 def add_to_similarity_family(base_msg_no, msg_no):
     """Add the given message number to the similarity family of the given base message number"""
     if base_msg_no not in similarity_families:
@@ -50,31 +39,31 @@ def get_embedding_with_caching(message_no, message_content):
         with open(f"{SCRATCH_DIR}/embedding.{message_no}.pkl", "rb") as picklefile:
             return pickle.load(picklefile)
     else:
-        embedding = model(message_content)
+        embedding = model([message_content])
         with open(f"{SCRATCH_DIR}/embedding.{message_no}.pkl", "wb") as picklefile:
             pickle.dump(embedding, picklefile)
         return embedding
 
+# First pass: compute any exact matches and prevent them from being computed again (except for base messages)
+for message in MESSAGES:
+    if message[0] in deemed_similar: continue
+    print(f"(exact pass) Base message {message[0]}: {message[10]}")
+    for other_message in MESSAGES:
+        if (message[0] == other_message[0]) or (message[0] in deemed_similar): continue
+        if message[10] == other_message[10]:
+            add_to_similarity_family(message[0], other_message[0])
+            deemed_similar.add(other_message[0]) # don't add message[0] because we still want to compute cosine similarity with it
+            print(f"Exact message {other_message[0]}: {other_message[10]}")
+
+# Second pass: compute cosine similarity with all other messages
 for message in MESSAGES:
     if message[0] in deemed_similar: continue
 
     print(f"Base message {message[0]}: {message[10]}")
     embedding = get_embedding_with_caching(message[0], message[10])
-    have_tried_exact_match = False # whether we've already tried to find an exact match for this base message
 
     for other_message in MESSAGES:
         if (message[0] == other_message[0]) or (other_message[0] in deemed_similar): continue
-
-        # If there's already been an exact match, it's more likely that there are
-        # even more exact metches
-        if (not have_tried_exact_match) and (message[10] == other_message[10]):
-            for exact_msg in try_exact_match(message[0], message[10]):
-                add_to_similarity_family(message[0], exact_msg)
-                deemed_similar.add(exact_msg)
-                print(f"Exact message {exact_msg}: {other_message[10]}")
-            add_to_similarity_family(message[0], message[0])
-            have_tried_exact_match = True
-            continue # don't compute cosine similarity for other_message since we know it's the same
 
         other_embedding = get_embedding_with_caching(other_message[0], other_message[10])
         if cosine_similarity(embedding, other_embedding)[0][0] > 0.8:
